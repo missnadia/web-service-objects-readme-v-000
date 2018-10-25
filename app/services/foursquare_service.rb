9fcdc0db33506f30cffebf20498cde5b134184ef
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-25 00:23:29 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -20,11 +20,12 @@ class FoursquareService
   end
 
   def foursquare(client_id, client_secret)
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
       req.params['client_secret'] = client_secret
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
+    end
   end
 end
